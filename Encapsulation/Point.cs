e8eddc70ce56ac65da229130715ac7bdc5fd8cec
ASUTP <ASUTP@DESKTOP-SSAKK7M>
2024-10-10 20:20:08 +0300

    operatorsDoNE

--- Encapsulation/Point.cs
+++ Encapsulation/Point.cs
@@ -55,10 +55,61 @@ namespace Encapsulation
         #endregion
         public double X { get; set; }
         public double Y { get; set; }
+        
+        public Point(double x=0, double y=0)
+        {
+            this.X = x;
+            this.Y = y;
+            Console.WriteLine($"DefaultConstructor:{this.GetHashCode()}");
+        }
+        public double Distance(Point othe)
+        {
+            double dx =X- othe.X;
+            double dy =Y - othe.Y;
+            return Math.Sqrt(dx * dx + dy * dy);
+        }
+        public Point(Point othe)
+        {
+            this.X = othe.X;
+                this.Y = othe.Y;
+            Console.WriteLine($"CopyConstructor:{this.GetHashCode()}");
+        }
+        ~Point()
+        {
+            Console.WriteLine($"destructor: \t{this.GetHashCode()}");
+        }
+        public static Point operator +(Point left, Point right)
+        {
+            Point res = new Point();
+            res.X = left.X + right.X;
+            res.Y = left.Y + right.Y;
+            return res;
+        }
+        public static Point operator -(Point left, Point right)
+        {
+            Point res = new Point();
+            res.X = left.X - right.X;
+            res.Y = left.Y - right.Y;
+            return res;
+        }
+        public static Point operator ++(Point obj)
+        {
+            obj.X++;
+            obj.Y++;
+            return obj;
+        }
+        public static bool operator ==(Point left,Point right)
+        {
+            return left.X == right.X && left.Y == right.Y;
+        }
+        public static bool operator !=(Point left, Point right)
+        {
+            return left.X != right.X && left.Y != right.Y;
+        }
         public void Print()
         {
             Console.WriteLine($" X = {X}\tY = {Y}");
         }
-       
+        
     }
 }
--- Encapsulation/Program.cs
+++ Encapsulation/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿//#define DISTANCE
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,7 @@ namespace Encapsulation
     {
         static void Main(string[] args)
         {
+#if DISTANCE
             Point A = new Point();
             //Console.WriteLine($" X = {A.GetX()}\tY = {A.GetY()}");
 
@@ -20,6 +22,25 @@ namespace Encapsulation
             A.X = 22;
             A.Y = 33;
             A.Print();
+            // Point B = new Point();
+            Point B = new Point(A);
+            B.Print();
+            Point C;
+            C = B;
+            C.Print(); 
+#endif
+            Point A = new Point(2, 3);
+            Point B = new Point(7, 8);
+            Point C = new Point(A + B);
+            C.Print();
+            Point D = new Point(A - B);
+            D.Print();
+            for(Point i=new Point();i.X<10;i++)
+            {
+                i.Print();
+            }
+            double F = A.Distance(B);
+            Console.WriteLine(F);
         }
     }
 }
